084f13dc6712e8417b167cf224cd16dbc62068df
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-06-10 20:44:31 +0000

    tekMapEnhancer - Fullscreen maps sucks, especially on widescreen.  Thanks Mikma for da code
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekMapEnhancer@433 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ tekMapEnhancer.lua
@@ -0,0 +1,7 @@
+﻿UIPanelWindows["WorldMapFrame"] = {area = "center", pushable = 9}
+hooksecurefunc(WorldMapFrame, "Show", function(self)
+	self:SetScale(0.75)
+	self:EnableKeyboard(false)
+	BlackoutWorld:Hide()
+end)
+MiniMapBattlefieldFrame.status = "active"
